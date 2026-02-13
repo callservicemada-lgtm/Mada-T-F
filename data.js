be15c188d121435a45878b24e0c7576770ccede0
callservicemada-lgtm <callservicemada@gmail.com>
2026-02-13 20:15:52 +0100

    Update and rename data.json to data.js

---
+++ data.js
@@ -0,0 +1,66 @@
+const db = [
+    {
+        sary: "https://images.unsplash.com/photo-1500382017468-9049fed747ef", 
+        vidy: "45.000.000 Ar", 
+        anarana: "Tany 500m² eny Imerintsiatosika", 
+        details: "Tany lemaka tsara hamboarana trano, efa misy rano sy jiro eo akaiky. Titré et Borné.", 
+        karazana: "tany", 
+        tel: "261340000000"
+    },
+    {
+        sary: "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf", 
+        vidy: "35.000.000 Ar", 
+        anarana: "Toyota Hilux Revo", 
+        details: "Mbola tena milamina, moteur impeccable, vao nampidirina avy any ivelany.", 
+        karazana: "fiara", 
+        tel: "261340000000"
+    },
+    {
+        sary: "https://images.unsplash.com/photo-1580587767303-9005085698b6", 
+        vidy: "120.000.000 Ar", 
+        anarana: "Villa F6 eny Ambohibao", 
+        details: "Trano be misy rihana, efitra fatoriana 4, tokotany malalaka sy zaridaina kanto.", 
+        karazana: "trano", 
+        tel: "261340000000"
+    },
+    {
+        sary: "https://images.unsplash.com/photo-1494976388531-d1058494cdd8", 
+        vidy: "18.000.000 Ar", 
+        anarana: "Mazda Demio - New Shape", 
+        details: "Fiara kely tsara ho an'ny an-tanàn-dehibe. Tsy mandany lasantsy, mbola madio be.", 
+        karazana: "fiara", 
+        tel: "261340000000"
+    },
+    {
+        sary: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750", 
+        vidy: "250.000.000 Ar", 
+        anarana: "Trano vato eny Antanimena", 
+        details: "Trano lehibe akaiky ny arabe, mety hatao birao na fonenana. Toerana azo antoka.", 
+        karazana: "trano", 
+        tel: "261340000000"
+    },
+    {
+        sary: "https://images.unsplash.com/photo-1500382017468-9049fed747ef", 
+        vidy: "15.000.000 Ar", 
+        anarana: "Tany 200m² eny Itaosy", 
+        details: "Tany mora vidy nefa akaiky ny toeram-pivarotana rehetra. Idealy ho an'ny tetikasa kely.", 
+        karazana: "tany", 
+        tel: "261340000000"
+    },
+    {
+        sary: "https://images.unsplash.com/photo-1542282088-fe8426682b8f", 
+        vidy: "22.000.000 Ar", 
+        anarana: "Hyundai Atos Prime", 
+        details: "Fiara tsotra sy matanjaka. Tsy mbola niasa teto Madagasikara (Occasion Europe).", 
+        karazana: "fiara", 
+        tel: "261340000000"
+    },
+    {
+        sary: "https://images.unsplash.com/photo-1506126613408-eca07ce68773", 
+        vidy: "65.000.000 Ar", 
+        anarana: "Tany malalaka eny Ivato", 
+        details: "Tany 1000m² akaiky ny seranam-piaramanidina. Mety tsara ho an'ny fampiasam-bola.", 
+        karazana: "tany", 
+        tel: "261340000000"
+    }
+];
